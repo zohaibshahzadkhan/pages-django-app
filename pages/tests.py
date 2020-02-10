a260c77c7774382fb17aeab0081d2f9d9d96f616
zohaibshahzadkhan <zohaibshahzad16@gmail.com>
2020-02-10 12:04:42 +0100

    Basic app with django 2.1

---
+++ pages/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+class MyTests(TestCase):
+  def test_home_page_status_code(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
+  def test_about_page_status_code(self):
+    response = self.client.get('/about/')
+    self.assertEqual(response.status_code, 200)
